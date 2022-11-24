7b4f0bcee7b6cf7fd492f86da3e2f7fb670bfc77
Gautam <garggautam22@gmail.com>
2022-11-24 08:26:35 +0530

    DocTalk

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -5,22 +5,36 @@ import More from "../img/more.png";
 import Messages from "./Messages";
 import Input from "./Input";
 import { ChatContext } from "../context/ChatContext";
+import { useState } from "react";
+import VideoConference from "./VideoConference";
 
 const Chat = () => {
   const { data } = useContext(ChatContext);
+
+  let [video, setVideo] = useState(false);
 
   return (
     <div className="chat">
       <div className="chatInfo">
         <span>{data.user?.displayName}</span>
         <div className="chatIcons">
-          <img src={Cam} alt="" />
+          <a
+            onClick={(e) => {
+              setVideo(true);
+            }}
+            href="#"
+          >
+            <img src={Cam} alt="" />
+          </a>
           <img src={Add} alt="" />
           <img src={More} alt="" />
         </div>
       </div>
+
+      {video && <VideoConference />}
+
       <Messages />
-      <Input/>
+      <Input />
     </div>
   );
 };
--- src/firebase.js
+++ src/firebase.js
@@ -4,16 +4,16 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "chat-ab746.firebaseapp.com",
-  projectId: "chat-ab746",
-  storageBucket: "chat-ab746.appspot.com",
-  messagingSenderId: "901216368405",
-  appId: "1:901216368405:web:8ec942ee51611df5c49b1c",
+  apiKey: "AIzaSyChomPCxt7c495F7Km9XSROYSDnX55NocM",
+  authDomain: "doctalk-cf8b8.firebaseapp.com",
+  projectId: "doctalk-cf8b8",
+  storageBucket: "doctalk-cf8b8.appspot.com",
+  messagingSenderId: "1025082903681",
+  appId: "1:1025082903681:web:88591aa94d155bbede1180",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const storage = getStorage();
-export const db = getFirestore()
+export const db = getFirestore();
